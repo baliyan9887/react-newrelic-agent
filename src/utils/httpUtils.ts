af1178359804a45b17da7127a3ece613076ac6a0
Gautam Kumar <gk@192.168.1.5>
2024-03-10 18:43:47 +0530

    chore: added httpUtils to make request to the relic

--- src/utils/httpUtils.ts
+++ src/utils/httpUtils.ts
@@ -1,6 +1,16 @@
+/**
+ * @file request.ts
+ * @description
+ * This module provides a function for making HTTP requests using Axios.
+ * It includes support for common request options, such as method, headers, URL, and body.
+ * Additionally, it checks whether the code is running on the server or in the browser and
+ * includes a polyfill for the fetch API if needed.
+ */
+
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
 import { RequestOptions } from '@/types'
 
+// Check if the code is running on the server or in the browser
 const isServer = typeof window === 'undefined'
 
 // Polyfill for fetch if it doesn't exist (browser-side)
@@ -9,7 +19,13 @@ if (!isServer && !('fetch' in window)) {
   require('whatwg-fetch')
 }
 
+/**
+ * Perform an HTTP request using Axios.
+ * @param options - The request options, including method, headers, URL, and body.
+ * @returns A Promise that resolves to the response data as a string.
+ */
 async function request(options: RequestOptions): Promise<string> {
+  // Convert the RequestOptions to AxiosRequestConfig
   const axiosConfig: AxiosRequestConfig = {
     method: options.method || 'GET',
     headers: options.headers || {},
@@ -19,20 +35,26 @@ async function request(options: RequestOptions): Promise<string> {
   }
 
   try {
+    // Make the HTTP request using Axios
     const response: AxiosResponse = await axios(axiosConfig)
 
+    // Check if the response is successful (status code 200)
     if (!response || response.status !== 200) {
       throw new Error(
         `HTTP error! Status: ${response ? response.status : 'unknown'}`
       )
     }
 
+    // Log the successful response to the console
     console.info('response:', response)
 
+    // Return the response data as a string
     return response.data as string
   } catch (error: any) {
+    // Handle errors during the Axios request
     throw new Error(`Axios request failed: ${error.message}`)
   }
 }
 
+// Export the request function as the default export
 export default request
